e8933b91de39204d01b0d1087e6e7049a0c7585b
Jonathan <cooler268@gmail.com>
2017-07-25 19:44:48 -0700

    project

--- Person2/src/person2/PhoneBook.java
+++ Person2/src/person2/PhoneBook.java
@@ -8,6 +8,10 @@ public class PhoneBook {
         length=0;
         PB = new Person2[100];
     }
-    
-            
+    public void addPerson(Person2 P)
+    {
+        PB[length]= P;
+        length++;
+    }
+    public void printEntries()
 }
