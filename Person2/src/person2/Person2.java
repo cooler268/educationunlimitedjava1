49679b70fc8e9a90d97ea99857432804c9003d1f
Jonathan <cooler268@gmail.com>
2017-07-25 19:07:24 -0700

    projects

--- Person2/src/person2/Person2.java
+++ Person2/src/person2/Person2.java
@@ -2,14 +2,40 @@ package person2;
 
 public class Person2 {
 
-    public static void (String[] args) {
-        int age =;
-        double height =;
-        String firstName =;
-        String lastName =;
-        boolean isAStudent =;
-                
-        
+        int age ;
+        double height ;
+        String firstName ;
+        String lastName ;
+        boolean isAStudent ;     
+
+    public String getFirstName() {
+        return firstName;
     }
-    
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+        String phoneNumber ; 
+        public Person2(String firstName, String lastName, String phoneNumber) {
+            firstName = firstName;
+            lastName = lastName;
+            phoneNumber = phoneNumber;
+           }
+        
 }
---
+++ Person2/src/person2/PhoneBook.java
@@ -0,0 +1,13 @@
+package person2;
+
+public class PhoneBook {
+    Person2[] PB;
+    int length;
+    
+    public PhoneBook(){
+        length=0;
+        PB = new Person2[100];
+    }
+    
+            
+}
