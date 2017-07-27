28803e2bdb68a30092c6ed10d9ec73b5dfb0e65d
Jonathan <cooler268@gmail.com>
2017-07-27 16:13:05 -0700

    project

--- FinalProject/src/finalproject/FinalProject.java
+++ FinalProject/src/finalproject/FinalProject.java
@@ -4,9 +4,12 @@ public class FinalProject {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Calculator:");
-        int integers = Integer.parseInt(keyboard.nextLine());
-        
+        System.out.println("Calculator:Enter numbers");
+        double decimals = Double.parseDouble(keyboard.nextLine());
+        for(int i = 0; i<decimals; i++) {
+            int[] array1 = 
+            System.out.println(decimals[i]);
+        }
     }
-    
+        
 }
